1fd20dff2634aaf36a2ec3fa7eb72f275eb63fb0
omalchielo <74373870+omalchielo@users.noreply.github.com>
2024-08-10 22:11:37 +0200

    Update listky.py

--- listky.py
+++ listky.py
@@ -8,7 +8,6 @@ import time
 
 url = "https://www.viagogo.com/Concert-Tickets/Country-and-Folk-Music/Taylor-Swift-Tickets/E-151949755?quantity=2&listingQty=&sortDirection=0&sortBy=NEWPRICE"
 
-
 def send_email(subject, content):
     email_sender = "botrozvrh@gmail.com"
     email_password = "ogkybntogxdmekzl"
@@ -25,7 +24,6 @@ def send_email(subject, content):
         smtp.login(email_sender, email_password)
         smtp.send_message(em)
 
-
 try:
     while True:
         response = requests.get(url)
@@ -36,6 +34,7 @@ try:
         json_matches = json_pattern.findall(html_content)
 
         ticket_details = ""
+        very_cheap_ticket_details = ""
         found_ticket = False
         found_very_cheap_ticket = False
 
@@ -47,7 +46,7 @@ try:
                         "rawMinPrice" in json_object and
                         "formattedMinPrice" in json_object):
 
-                    # Check if the price is below the threshold of 11500
+                    # Check if the price is below the threshold of 450
                     if json_object["rawMinPrice"] < 450:
                         found_ticket = True
                         # Accumulate ticket details
@@ -58,18 +57,25 @@ try:
                             f"Link: {url}\n\n"
                         )
 
-                    # Check if the price is below the threshold of 11050
+                    # Check if the price is below the threshold of 350
                     if json_object["rawMinPrice"] < 350:
                         found_very_cheap_ticket = True
+                        # Accumulate very cheap ticket details
+                        very_cheap_ticket_details += (
+                            f"Count: {json_object['count']}\n"
+                            f"Price: {json_object['formattedMinPrice']}\n"
+                            f"Raw Price: {json_object['rawMinPrice']}\n"
+                            f"Link: {url}\n\n"
+                        )
 
             except json.JSONDecodeError:
                 continue
 
         if found_ticket:
-            send_email("Tickets Found Below 11500", f"Tickets under 11500 found:\n\n{ticket_details}")
+            send_email("Tickets Found Below 450", f"Tickets under 450 found:\n\n{ticket_details}")
 
         if found_very_cheap_ticket:
-            send_email("We Got It", "We got it! Tickets found under 10000.")
+            send_email("We Got It", f"We got it! Tickets found under 350:\n\n{very_cheap_ticket_details}")
 
         time.sleep(480)
 
@@ -90,6 +96,5 @@ except Exception as e:
         with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as smtp:
             smtp.login(email_sender, email_password)
             smtp.send_message(em)
-
 
     send_status_email()
